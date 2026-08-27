161497e66d8f1073de52509fdce129a0d5552c7e
Liv Hong <lilixinxin@protonmail.com>
2026-08-27 09:56:03 +0700

    feat: improve control plane security group

---
+++ terraform/aws/ec2/sg.tf
@@ -0,0 +1,117 @@
+# --- Security group -------------------------------------------------------
+# One shared SG for all k0s nodes. Rules are intentionally scoped tight:
+# SSH only from your IP, k0s API (9443) and kubelet (10250) only between
+# nodes in this SG, everything else outbound-only.
+
+locals {
+  control_group_id = aws_security_group.talos_control_nodes.id
+  worker_group_id  = aws_security_group.talos_worker_nodes.id
+}
+
+resource "aws_security_group" "talos_control_nodes" {
+  name_prefix = "${var.environment}-talos-control"
+  vpc_id      = var.vpc_id
+
+  tags = {
+    Name = "${var.environment}-control-talos-sg"
+  }
+}
+
+resource "aws_security_group" "talos_worker_nodes" {
+  name_prefix = "${var.environment}-talos-worker"
+  vpc_id      = var.vpc_id
+
+  tags = {
+    Name = "${var.environment}-worker-talos-sg"
+  }
+}
+
+## Control Plane Security Group ##
+
+resource "aws_vpc_security_group_egress_rule" "egress_all" {
+  security_group_id = local.control_group_id
+
+  cidr_ipv4   = "0.0.0.0/0"
+  ip_protocol = "-1"
+}
+
+
+
+resource "aws_vpc_security_group_ingress_rule" "kubelet" {
+  security_group_id = local.control_group_id
+
+  referenced_security_group_id = local.control_group_id
+  from_port                    = 10250
+  ip_protocol                  = "tcp"
+  to_port                      = 10250
+}
+
+resource "aws_vpc_security_group_ingress_rule" "controller_manager" {
+  security_group_id = local.control_group_id
+
+  referenced_security_group_id = local.control_group_id
+  from_port                    = 10257
+  ip_protocol                  = "tcp"
+  to_port                      = 10257
+}
+
+resource "aws_vpc_security_group_ingress_rule" "shceduler" {
+  security_group_id = local.control_group_id
+
+  referenced_security_group_id = local.control_group_id
+  from_port                    = 10259
+  ip_protocol                  = "tcp"
+  to_port                      = 10259
+}
+
+resource "aws_vpc_security_group_ingress_rule" "apiserver-etcd" {
+  security_group_id = local.control_group_id
+
+  referenced_security_group_id = local.control_group_id
+  from_port                    = 2379
+  ip_protocol                  = "tcp"
+  to_port                      = 2380
+}
+
+resource "aws_vpc_security_group_ingress_rule" "kube-apiserver" {
+  security_group_id = local.control_group_id
+
+  cidr_ipv4   = var.ssh_ingress_cidr
+  from_port   = 6443
+  ip_protocol = "tcp"
+  to_port     = 6443
+}
+
+resource "aws_vpc_security_group_ingress_rule" "trustd" {
+  security_group_id = local.control_group_id
+
+  referenced_security_group_id = local.worker_group_id
+  from_port                    = 50001
+  ip_protocol                  = "tcp"
+  to_port                      = 50001
+
+  description = "Talos trustd service. SHould be open on control plane from workers. Based on v1.13 documentation https://docs.siderolabs.com/talos/v1.13/learn-more/talos-network-connectivity"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "apid_self" {
+  security_group_id = local.control_group_id
+
+  referenced_security_group_id = local.control_group_id
+  from_port                    = 50000
+  ip_protocol                  = "tcp"
+  to_port                      = 50000
+
+  description = "Talos apid to provide for Talosctl access. Based on v1.13 documentation https://docs.siderolabs.com/talos/v1.13/learn-more/talos-network-connectivity"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "apid_master" {
+  security_group_id = local.control_group_id
+
+  cidr_ipv4   = var.ssh_ingress_cidr
+  from_port   = 50000
+  ip_protocol = "tcp"
+  to_port     = 50000
+
+  description = "Talos apid to provide for Talosctl access. Based on v1.13 documentation https://docs.siderolabs.com/talos/v1.13/learn-more/talos-network-connectivity"
+}
+## Worker Plane Security Group ##
